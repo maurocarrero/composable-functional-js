82f802383122ef51b97d3985c4f5dfb862032caf
Mauro Carrero <mauro.carrero@sabre.com>
2017-04-24 14:23:47 -0300

    [Fix]
    File names were not updated.

--- src/money-to-float/index.js
+++ src/money-to-float/index.js
@@ -2,8 +2,8 @@ console.log('*******************************************************************
 console.log('Money to float')
 console.log('*********************************************************************************************************')
 
-const applyDiscount = require('./declarative')
-const composedApplyDiscount = require('./composition')
+const applyDiscount = require('./imperative')
+const composedApplyDiscount = require('./composed')
 
 const imperativeResult = applyDiscount('$5.00', '20%')
 const composedResult = composedApplyDiscount('$5.00', '20%')

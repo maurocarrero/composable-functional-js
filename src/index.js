064c40d642bffffe1082fcf0cf5314ede2a2e399
Mauro Carrero <maurocarrero@lataperainvisible.com.uy>
2017-04-25 19:43:22 -0300

    [readline]
    Adds readline to ask for the sample to choose

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-require('./next-char-from-number-string')
-require('./money-to-float')
-require('./either-type')
+const args = process.argv
+
+const parseAnswer = (answer) => {
+  switch (answer) {
+    case '1':
+      require('./next-char-from-number-string')
+      break
+    case '2':
+      require('./money-to-float')
+      break
+    case '3':
+      require('./either-type')
+    default:
+      console.log('Not a valid option.')
+  }
+}
+
+if (args.length > 2) {
+  parseAnswer(args[ 2 ])
+} else {
+  console.log('*******************************************************')
+  console.log('*******************************************************')
+  console.log('1. Next char from number string')
+  console.log('2. Money to float')
+  console.log('3. Either Type')
+  console.log('')
+
+  const readline = require('readline')
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Sample [ 1 | 2 | 3 ]? ', answer => {
+    parseAnswer(answer)
+    rl.close()
+  })
+}
